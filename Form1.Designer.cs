8738a7fe11663338e4d98c1aeef4d15064b524bf
luiseduardomattos <114542913+luiseduardomattos@users.noreply.github.com>
2023-03-01 21:42:18 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace PICTURE_BOX
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBoxMostrar = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBoxMostrar2 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMostrar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMostrar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxMostrar
+            // 
+            this.pictureBoxMostrar.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxMostrar.Image")));
+            this.pictureBoxMostrar.Location = new System.Drawing.Point(56, 12);
+            this.pictureBoxMostrar.Name = "pictureBoxMostrar";
+            this.pictureBoxMostrar.Size = new System.Drawing.Size(237, 145);
+            this.pictureBoxMostrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxMostrar.TabIndex = 0;
+            this.pictureBoxMostrar.TabStop = false;
+            this.pictureBoxMostrar.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(56, 163);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(237, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "MOSTRAR IMAGEM";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(56, 192);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(237, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "APAGAR IMAGEM";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBoxMostrar2
+            // 
+            this.pictureBoxMostrar2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxMostrar2.Image")));
+            this.pictureBoxMostrar2.Location = new System.Drawing.Point(516, 12);
+            this.pictureBoxMostrar2.Name = "pictureBoxMostrar2";
+            this.pictureBoxMostrar2.Size = new System.Drawing.Size(231, 145);
+            this.pictureBoxMostrar2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxMostrar2.TabIndex = 3;
+            this.pictureBoxMostrar2.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(516, 172);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(231, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "MOSTRAR IMAGEM2";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(516, 201);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(231, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "APAGAR IMAGEM2";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(193, 292);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(352, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "FECHAR TODAS IMAGENS";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(193, 252);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(352, 23);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "MOSTRAR TODAS AS IMAGENS";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBoxMostrar2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBoxMostrar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMostrar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMostrar2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxMostrar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBoxMostrar2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PICTURE_BOX
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.pictureBoxMostrar.Visible = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.pictureBoxMostrar.Visible = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.pictureBoxMostrar2.Visible = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.pictureBoxMostrar2.Visible = true;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            this.pictureBoxMostrar.Visible = true; this.pictureBoxMostrar2.Visible = true;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.pictureBoxMostrar.Visible = false; this.pictureBoxMostrar2.Visible = false;
+        }
+    }
+}
